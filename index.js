691a23a140334116a908028fa17d7d91f3ed7e8d
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-13 22:09:11 +0600

    delete data from Db

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {MongoClient, ServerApiVersion} = require('mongodb');
+const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -44,6 +44,14 @@ async function run() {
           const result = await usersCollection.insertOne(newUser);
           res.send(result)
         } )
+
+        // Delete
+        app.delete('/users/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await usersCollection.deleteOne(query);
+          res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
