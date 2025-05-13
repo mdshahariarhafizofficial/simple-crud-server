740d3638ee96db038452d88fc0b157471127c936
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-13 21:05:44 +0600

    create and get data from DB

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-
 // Middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // Db info
 // user: simpleDbUser
 // Pass: CBNP4kmhFG4fpNd9
+
 const uri = "mongodb+srv://simpleDbUser:CBNP4kmhFG4fpNd9@cluster0.fkarqx9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +26,25 @@ const client = new MongoClient(uri, {
 async function run() {
     try{
         await client.connect();
+
+        const database = client.db("usersDb");
+        const usersCollection = database.collection("users")
+
+        // Get
+        app.get('/users', async (req, res) => {
+          const cursor = usersCollection.find();
+          const result = await cursor.toArray();
+          res.send(result)
+        } )
+
+        // Post
+        app.post('/users', async (req, res) => {
+          console.log("data added on DB", req.body);
+          const newUser = req.body;
+          const result = await usersCollection.insertOne(newUser);
+          res.send(result)
+        } )
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
