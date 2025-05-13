b92fe22058177b2adb054ab71f9adfbe13f8551b
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-13 13:21:16 +0600

    server successfully connected to MongoDB!

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const {MongoClient, ServerApiVersion} = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
 
 
 // Middleware
@@ -11,6 +13,30 @@ app.use(express.json())
 // Db info
 // user: simpleDbUser
 // Pass: CBNP4kmhFG4fpNd9
+const uri = "mongodb+srv://simpleDbUser:CBNP4kmhFG4fpNd9@cluster0.fkarqx9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+    try{
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }finally{
+
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('Hlw Dev');
